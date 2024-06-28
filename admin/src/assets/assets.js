5f484880a7134d7fbb8d7cf823287f1987cabbe3
Sabari Vijayan <sabari25112012@gmail.com>
2024-06-28 17:41:00 +0530

    updated price symbol and delivery charges

--- admin/src/assets/assets.js
+++ admin/src/assets/assets.js
@@ -6,7 +6,7 @@ import upload_area from './upload_area.png'
 import parcel_icon from './parcel_icon.png'
 
 export const url = 'http://localhost:4000'
-export const currency = '₹'
+export const currency = '$'
 
 export const assets ={
     logo,
